289347eb89bb63f40734dbc3261709e430b420bd
KatyaRa17 <sosninae17@gmail.com>
2024-11-13 14:34:07 +0500

    Add files via upload

---
+++ Money.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace лаба2_23
+{
+    public class Money
+    {
+        private uint rubles;
+        private byte kopeks;
+
+        public uint Rubles
+        {
+            get { return rubles; }
+            private set { rubles = value; }
+        }
+
+        public byte Kopeks
+        {
+            get { return kopeks; }
+            private set { kopeks = value; }
+        }
+
+        // Конструктор
+        public Money(uint rubles, byte kopeks)
+        {
+            if (kopeks >= 100)
+            {
+                throw new ArgumentException("Копейки должны быть меньше 100");
+            }
+
+            this.rubles = rubles;
+            this.kopeks = kopeks;
+        }
+
+        // Метод ToString()
+        public override string ToString()
+        {
+            return $"{rubles} руб. {kopeks} коп.";
+        }
+
+        // Метод добавления копеек
+        public Money AddKopeks(uint additionalKopeks)
+        {
+            if (additionalKopeks < 0)
+            {
+                throw new ArgumentException("Количество копеек должно быть неотрицательным");
+            }
+
+            uint totalKopeks = kopeks + additionalKopeks;
+            uint newRubles = rubles + (totalKopeks / 100);
+            byte newKopeks = (byte)(totalKopeks % 100);
+
+            return new Money(newRubles, newKopeks);
+        }
+
+        // Перегрузка операции ++
+        public static Money operator ++(Money money)
+        {
+            return money.AddKopeks(1);
+        }
+
+        // Перегрузка операции --
+        public static Money operator --(Money money)
+        {
+            if (money.Kopeks == 0 && money.Rubles == 0)
+            {
+                throw new InvalidOperationException("Нельзя вычесть копейку из 0.");
+            }
+
+            uint totalKopeks = (money.Rubles * 100) + money.Kopeks - 1;
+            uint newRubles = totalKopeks / 100;
+            byte newKopeks = (byte)(totalKopeks % 100);
+
+            return new Money(newRubles, newKopeks);
+        }
+
+        // Перегрузка операции приведения типа uint
+        public static explicit operator uint(Money money)
+        {
+            return money.Rubles;
+        }
+
+        // Перегрузка операции приведения типа double
+        public static implicit operator double(Money money)
+        {
+            return ((double)money.Kopeks / 100) + money.Rubles;
+        }
+
+        // Перегрузка операции сложения
+        public static Money operator +(Money money, uint value)
+        {
+            return money.AddKopeks(value);
+        }
+
+        //Перегрузка операции сложения(коммутативная)
+        public static Money operator +(uint value, Money money)
+        {
+            return money + value;
+        }
+
+        //Перегрузка операции вычитания
+            public static Money operator -(Money money, uint value)
+        {
+            uint totalKopeks = (money.Rubles * 100) + money.Kopeks - value;
+            if (totalKopeks < 0)
+            {
+                throw new InvalidOperationException("Результат вычитания не может быть отрицательным.");
+            }
+
+            uint newRubles = totalKopeks / 100;
+            byte newKopeks = (byte)(totalKopeks % 100);
+
+            return new Money(newRubles, newKopeks);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace лаба2_23
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите сумму в рублях:");
+                uint rubles = uint.Parse(Console.ReadLine());
+                Console.WriteLine("Введите сумму в копейках:");
+                byte kopeks = byte.Parse(Console.ReadLine());
+
+                Money money = new Money(rubles, kopeks);
+                Console.WriteLine("Создан объект Money: " + money.ToString());
+
+                Console.WriteLine("Введите количество копеек для добавления:");
+                uint additionalKopeks = uint.Parse(Console.ReadLine());
+
+                Money newMoney = money.AddKopeks(additionalKopeks);
+                Console.WriteLine("Новая сумма: " + newMoney.ToString());
+
+                // Тестирование операций ++ и --
+                Console.WriteLine("Тест операции ++: " + (++newMoney).ToString());
+
+                Console.WriteLine("Тест операции --: " + (--newMoney).ToString());
+
+                // Тестирование операции приведения типа uint
+                uint moneyAsUint = (uint)newMoney;
+                Console.WriteLine("Тест операции приведения к uint: " + moneyAsUint);
+
+                // Тестирование операции приведения типа double
+                double moneyAsDouble = newMoney;
+                Console.WriteLine("Тест операции приведения к double: " + moneyAsDouble);
+
+                // Тестирование операции сложения (Money + uint)
+                Console.WriteLine("Введите число для сложения:");
+                uint value = uint.Parse(Console.ReadLine());
+                Money sum = newMoney + value;
+                Console.WriteLine("Сумма: " + sum.ToString());
+
+                // Тестирование операции сложения (uint + Money)
+                Money sum2 = value + newMoney;
+                Console.WriteLine("Сумма: " + sum2.ToString());
+
+                // Тестирование операции вычитания (Money - uint)
+                Console.WriteLine("Введите число для вычитания:");
+                uint subtrahend = uint.Parse(Console.ReadLine());
+                Money diff = newMoney - subtrahend;
+                Console.WriteLine("Разность: " + diff.ToString());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: неверный формат ввода!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+
+}
